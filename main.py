2a56fef4a1e131f3bfecb0e20440389df1755f8b
pisakhov <pisakhov@gmail.com>
2025-10-02 18:22:22 -0400

    Refactor frontend to use Jinja2 templating
    Migrated static HTML to Jinja2 templates for better structure and maintainability. Updated FastAPI route to render templates with context, and moved shared head and style elements to base.html for reuse. This enables dynamic content and cleaner template inheritance.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from pathlib import Path
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="frontend"), name="static")
+templates = Jinja2Templates(directory="frontend")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
-    html_file = Path("frontend/home/index.html")
-    return html_file.read_text()
+async def read_root(request: Request):
+    return templates.TemplateResponse("home/index.html", {"request": request})
 
 if __name__ == "__main__":
     import uvicorn
